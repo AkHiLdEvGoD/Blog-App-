3f4d7d08a1caabf5ed4eb0aa153129d0ba691e4e
Akhil <akhdean@gmail.com>
2024-10-29 16:59:10 +0530

    Added home page, Base template and model

--- BlogApp/Blog/models.py
+++ BlogApp/Blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
+
+class Blog(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    blog_area = models.TextField()
+    time_date_posted = models.DateTimeField(default=timezone.now)
+    image =  models.ImageField(upload_to='blogPics/',blank=True,null=True)
+
+    def __str__(self):
+        return f'{self.user.username} posted on {self.time_date_posted}'
